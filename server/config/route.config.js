f62e31bd94ae62219eb70efd26be9add4c36569f
ishi12121 <isht1402@gmail.com>
2022-10-12 23:05:00 +0530

    completed user middleware for user authentication

---
+++ server/config/route.config.js
@@ -0,0 +1,31 @@
+import passport from 'passport';
+import JwtPassport from 'passport-jwt'
+
+import { UserModel } from '../database/allModels'
+
+const JWTStrategy = JwtPassport.Strategy;
+const ExtractJwt = JwtPassport.ExtractJwt;
+
+// Head:{
+// Auhtorization: "Bearer ancjanksnnxlsclaxk83ud3io"
+// }
+const options = {
+ jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+ secretOrKey: "ZomatoApp",
+
+};
+
+export default (passport) => {
+    passport.use(
+        new JWTStrategy(options, async (jwt__payload, done) => {
+         try{
+            const doesUserExist = await UserModel.findById(jwt__payload.user);
+            if(!doesUserExist) return done(null,false);
+            return done(null, doesUserExist);
+         } catch(error){
+            throw new Error(error);
+         }
+        })
+    );
+};
+ 
