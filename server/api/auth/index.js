5696a76891c1d120953f7684e367bff22c5d0aca
ishi12121 <isht1402@gmail.com>
2022-10-19 20:58:15 +0530

    completed server

--- server/api/auth/index.js
+++ server/api/auth/index.js
@@ -1,7 +1,11 @@
 import express from "express";
+import routeConfig from "../../config/route.config";
 
 import { UserModel } from "../../database/allModels";
 import { ValidateSignin, ValidateSignup } from "../../validation/auth";
+
+import passport from "passport";
+
 
 const Router = express.Router();
 
@@ -45,5 +49,27 @@ Router.post("/signin", async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 });
+
+Router.get('/google', passport.authenticate('google', {
+   scope: [
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/userinfo.email",
+
+   ]
+}))
+
+Router.get(
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // return res.status(200).json({
+    //   token: req.session.passport.user.token,
+    // });
+
+    return res.redirect(
+      `http://localhost:3000/google/${req.session.passport.user.token}`
+    );
+  }
+);
 
 export default Router;
--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import session from "express-session";
 
 // Private route authorization config
 import privateRouteConfig from "./config/route.config";
+import googleAuthConfig from './config/google.config'
 
 // Database connection
 import ConnectDB from "./database/connection";
@@ -25,6 +26,7 @@ const zomato = express();
 
 // adding additional passport configuration
 privateRouteConfig(passport);
+googleAuthConfig(passport);
 
 zomato.use(express.json());
 zomato.use(session({secret: process.env.JWTSECRET}));
