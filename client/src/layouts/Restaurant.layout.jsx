206905ecd95228e67eaaac57cdfaab5516c21349
ishi12121 <isht1402@gmail.com>
2022-10-23 11:42:59 +0530

    setted up routing system

---
+++ client/src/layouts/Restaurant.layout.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import {TistarOutline} from 'react-icons/ti';
+import {RiDirectionLine, RiShareForwardLine } from 'react-icons/ri';
+import {BiBookmarkPlus} from 'react-icons/bi';
+
+// components
+import Navbar from '../components/Navbar';
+// import ImageGrid from "../components/Restaurant/ImageGrid";
+// import InfoButton from "../components/Restaurant/InfoButton";
+// import RestaurantInfo from "../components/Restaurant/RestautantInfo";
+// import Tabs from '../components/Cart/CartContainer';
+// import CardContainer from "../components/Cart/CartContainer";
+const RestaurantLayout =
+(Component) => 
+({...props}) => {
+  return (
+   <>
+    <Navbar />
+    <Component {...props}/>
+   
+   </>
+  )
+};
+
+export default RestaurantLayout;
+
+{/*   <div className='container mx-auto px-4 mt-8 lg:px-20 pb-20'>
+ <ImageGrid images={} /> 
+ <RestaurantInfo name='' restaurantRating='' deliveryRating='' cuisine='' address=''  />
+ <div className='my-4 flex flex-wrap gap-3 mx-auto'>
+       <InfoButton isActive='true'>
+         <TistarOutline/> Add Review
+       </InfoButton>
+       <InfoButton>
+       <RiDirectionLine/> Direction
+       </InfoButton>
+       <InfoButton >
+       <BiBookmarkPlus/> Bookmark
+     </InfoButton>
+     <InfoButton >
+     <RiShareForwardLine/> Share
+     </InfoButton>
+     
+ </div>
+     <div className='my-10'>
+       <Tabs  />
+     
+     </div>
+ <Component {...props} />
+
+</div>
+<CardContainer/> */}
---
+++ client/src/pages/Home.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HomepageLayout from '../layouts/Homepage.layout';
+// layout
+
+
+
+const Home = () => {
+  return (
+    <div>Home</div>
+  )
+};
+
+export default HomepageLayout(Home);
