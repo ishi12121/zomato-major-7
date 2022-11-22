f498bed188563e925f245db48467b0c7e6d813fe
ishi12121 <isht1402@gmail.com>
2022-11-22 16:34:31 +0530

    minor changes

--- client/src/components/Auth/Signup.jsx
+++ client/src/components/Auth/Signup.jsx
@@ -35,7 +35,7 @@ const Signup = ({ isOpen, setIsOpen }) => {
   };
 
   const googleSignUp = () =>
-    (window.location.href = "http://localhost:4000/auth/google");
+  (window.location.href = `${process.env.REACT_APP_CLIENT_URL}auth/google`);
 
   return (
     <>
